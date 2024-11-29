db397fdcb0660202f97cc21d5c83b922338ec748
Antucan <antherpra@gmail.com>
2024-11-29 19:30:21 +0100

    in the first issue

--- src/Intervention.php
+++ src/Intervention.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 error_reporting(ENT_IGNORE);
 //print_r($_FILES["imageFile"]);
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'uf2/joseportugal',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a22b188e9527c6e108d2daf80834c4fce63cde2d',
+        'reference' => '71862fbc1b72c638607c1a3d5e3c2413259790bb',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -72,7 +72,7 @@
         'uf2/joseportugal' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a22b188e9527c6e108d2daf80834c4fce63cde2d',
+            'reference' => '71862fbc1b72c638607c1a3d5e3c2413259790bb',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
